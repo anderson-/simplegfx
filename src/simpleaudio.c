e70b1acebb1f9904cc11ceabbf8798bcdb6b5e0f
Anderson Antunes <anderson.utf@gmail.com>
2026-06-12 17:53:32 -0300

    perfeito

--- src/simpleaudio.c
+++ src/simpleaudio.c
@@ -14,7 +14,7 @@ typedef struct {
   int vol;
 } chan_t;
 
-static chan_t *chans[GFXA_CHANNELS];
+static chan_t chans[GFXA_CHANNELS];
 
 static void apply_fade_out(int16_t *buf, int n) {
   for (int i = 0; i < n; i++)
@@ -24,8 +24,8 @@ static void apply_fade_out(int16_t *buf, int n) {
 static int _mix_fill(int16_t *out, int n, void *user) {
   for (int i = 0; i < n; i++) out[i] = 0;
   for (int c = 0; c < GFXA_CHANNELS; c++) {
-    chan_t *ch = chans[c];
-    if (!ch) continue;
+    chan_t *ch = &chans[c];
+    if (!ch->playing) continue;
 
     int v = ch->vol;
     if (v <= 1) {
@@ -37,13 +37,8 @@ static int _mix_fill(int16_t *out, int n, void *user) {
     }
 
     if (!ch->fn) {
-      free(ch);
-      chans[c] = NULL;
+      memset(ch, 0, sizeof(chan_t));
       continue;
-    }
-
-    if (ch->playing) {
-      ch->playing = 0;
     }
 
     int r = ch->fn(ch->buf, n, ch->data);
@@ -67,33 +62,52 @@ int gfxa_play(audio_stream_t fn, void *data, int channel) {
   if (!fn) return -1;
 
   int c = channel;
-  if (c < 0)
-    for (int i = 0; i < GFXA_CHANNELS; i++)
-      if (!chans[i]) { c = i; break; }
-  if (c < 0 || c >= GFXA_CHANNELS) return -1;
+  if (c < 0) {
+    for (int i = 0; i < GFXA_CHANNELS; i++) {
+      if (!chans[i].playing) {
+        c = i;
+        break;
+      }
+    }
+  }
 
-  chan_t *ch = chans[c];
-  if (!ch) {
-    ch = chans[c] = (chan_t *)malloc(sizeof(chan_t));
-    if (!ch) return -1;
+  if (c < 0 || c >= GFXA_CHANNELS) return -1;
+  chan_t *ch = &chans[c];
+  if (!ch->playing) {
     memset(ch, 0, sizeof(chan_t));
+    ch->playing = 1;
     ch->fn = fn;
     ch->data = data;
     ch->vol = 1;
-    fn(ch->buf, GFXA_BUF_SIZE, data);
-    for (int i = 0; i < GFXA_BUF_SIZE; i++)
-      ch->buf[i] = (int16_t)((int32_t)ch->buf[i] * i / GFXA_BUF_SIZE);
+    int r = fn(ch->buf, GFXA_BUF_SIZE, data);
+    if (r < GFXA_BUF_SIZE) {
+      for (int i = r; i < GFXA_BUF_SIZE; i++)
+        ch->buf[i] = 0;
+      ch->fn = NULL;
+    }
+    for (int i = 0; i < GFXA_BUF_SIZE; i++) {
+      ch->buf[i] = (int16_t)(
+        (int32_t)ch->buf[i] * i / GFXA_BUF_SIZE
+      );
+    }
   } else {
     int16_t tmp[GFXA_BUF_SIZE];
     memcpy(tmp, ch->buf, GFXA_BUF_SIZE * sizeof(int16_t));
     ch->fn = fn;
     ch->data = data;
-    fn(ch->buf, GFXA_BUF_SIZE, data);
-    for (int i = 0; i < GFXA_BUF_SIZE; i++)
-      ch->buf[i] = (int16_t)((int32_t)tmp[i] * (GFXA_BUF_SIZE - i) / GFXA_BUF_SIZE
-                           + (int32_t)ch->buf[i] * i / GFXA_BUF_SIZE);
+    int r = fn(ch->buf, GFXA_BUF_SIZE, data);
+    if (r < GFXA_BUF_SIZE) {
+      for (int i = r; i < GFXA_BUF_SIZE; i++)
+        ch->buf[i] = 0;
+      ch->fn = NULL;
+    }
+    for (int i = 0; i < GFXA_BUF_SIZE; i++) {
+      ch->buf[i] = (int16_t)(
+        (int32_t)tmp[i] * (GFXA_BUF_SIZE - i) / GFXA_BUF_SIZE +
+        (int32_t)ch->buf[i] * i / GFXA_BUF_SIZE
+      );
+    }
   }
-  ch->playing = 1;
 
   if (started) return c;
   started = 1;
@@ -102,6 +116,7 @@ int gfxa_play(audio_stream_t fn, void *data, int channel) {
 }
 
 static void chan_stop(chan_t *ch) {
+  if (!ch->playing) return;
   apply_fade_out(ch->buf, GFXA_BUF_SIZE);
   ch->fn = NULL;
 }
@@ -109,11 +124,11 @@ static void chan_stop(chan_t *ch) {
 void gfxa_stop(int channel) {
   if (channel < 0) {
     for (int c = 0; c < GFXA_CHANNELS; c++)
-      if (chans[c]) chan_stop(chans[c]);
+      chan_stop(&chans[c]);
     return;
   }
   if (channel >= GFXA_CHANNELS) return;
-  if (chans[channel]) chan_stop(chans[channel]);
+  chan_stop(&chans[channel]);
 }
 
 void gfxa_wait(int channel, int ms) {
@@ -121,9 +136,13 @@ void gfxa_wait(int channel, int ms) {
   while (ms > 0) {
     int any = 0;
     if (channel < 0) {
-      for (int c = 0; c < GFXA_CHANNELS; c++)
-        if (chans[c]) { any = 1; break; }
-    } else if (channel < GFXA_CHANNELS && chans[channel]) {
+      for (int c = 0; c < GFXA_CHANNELS; c++) {
+        if (chans[c].playing) {
+          any = 1;
+          break;
+        }
+      }
+    } else if (channel < GFXA_CHANNELS && chans[channel].playing) {
       any = 1;
     }
     if (!any) break;
@@ -131,7 +150,8 @@ void gfxa_wait(int channel, int ms) {
     ms--;
     waited = 1;
   }
-  if (waited) gfx_delay(GFXA_BUF_SIZE * 1000 / GFXA_SAMPLE_RATE);
+  if (waited)
+    gfx_delay(GFXA_BUF_SIZE * 1000 / GFXA_SAMPLE_RATE);
 }
 
 void gfxa_set_volume(int channel, int vol) {
@@ -141,7 +161,8 @@ void gfxa_set_volume(int channel, int vol) {
     return;
   }
   if (channel >= GFXA_CHANNELS) return;
-  if (chans[channel]) chans[channel]->vol = vol;
+  if (chans[channel].playing)
+    chans[channel].vol = vol;
 }
 
 typedef struct { int freq, sr, pos, len; } beep_t;
@@ -161,8 +182,8 @@ void gfx_beep(int freq, int ms) {
   int sr = GFXA_SAMPLE_RATE;
   int n = sr * ms / 1000;
   if (n < 2 * GFXA_BUF_SIZE) n = 2 * GFXA_BUF_SIZE;
-  beep_t *state = malloc(sizeof(beep_t));
-  *state = (beep_t){freq, sr, 0, n};
-  gfxa_wait(gfxa_play(_beep_fill, state, -1), ms);
-  free(state);
+  beep_t state = { freq, sr, 0, n };
+  int ch = gfxa_play(_beep_fill, &state, -1);
+  if (ch >= 0)
+    gfxa_wait(ch, ms);
 }
